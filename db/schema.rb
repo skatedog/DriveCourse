14dca4cf461363321e0413803cfdb39ec9485c05
skatedog <skatedog0001@gmail.com>
2021-11-05 13:42:25 +0000

    [Add]Genreモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_015523) do
+ActiveRecord::Schema.define(version: 2021_11_05_133012) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+# サンプルGenre
+Genre.create!([
+  { name: "道の駅"},
+  { name: "飲食店"},
+  { name: "温泉"}
+])
